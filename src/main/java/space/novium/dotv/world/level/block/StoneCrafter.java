ac663d81d5aa0a93a4b35b0bc3ac783b8f7d0466
Novium <nova@novium.space>
2024-03-06 23:36:59 -0700

    Stone Crafter
    This is a simple combination crafter that will be used to get the most basic items to get started in the pack

--- src/main/java/space/novium/dotv/world/level/block/StoneCrafter.java
+++ src/main/java/space/novium/dotv/world/level/block/StoneCrafter.java
@@ -1,12 +1,17 @@
 package space.novium.dotv.world.level.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 import space.novium.dotv.world.level.block.entity.StoneCrafterBlockEntity;
 
@@ -22,4 +27,19 @@ public class StoneCrafter extends Block implements EntityBlock {
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state){
         return new StoneCrafterBlockEntity(pos, state);
     }
+    
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit){
+        BlockEntity block = level.getBlockEntity(pos);
+        if(block instanceof StoneCrafterBlockEntity crafter){
+            boolean mainHandEmpty = player.getMainHandItem().isEmpty();
+            if(mainHandEmpty){
+                return crafter.tryRemoveLastItem(player);
+            } else {
+                return crafter.tryAddItem(player, hand, pos);
+            }
+        } else {
+            return InteractionResult.PASS;
+        }
+    }
+    
 }
--- src/main/java/space/novium/dotv/world/level/block/entity/StoneCrafterBlockEntity.java
+++ src/main/java/space/novium/dotv/world/level/block/entity/StoneCrafterBlockEntity.java
@@ -1,14 +1,39 @@
 package space.novium.dotv.world.level.block.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import space.novium.dotv.setup.registration.ModBlockEntities;
 
 public class StoneCrafterBlockEntity extends BlockEntity {
-    public static int MAX_ITEMS = 8;
+    private final SimpleContainer items = new SimpleContainer(1){
+        public int getMaxStackSize(){
+            return 1;
+        }
+    };
     
     public StoneCrafterBlockEntity(BlockPos pos, BlockState state){
         super(ModBlockEntities.STONE_CRAFTER_ENTITY.get(), pos, state);
+    }
+    
+    public InteractionResult tryRemoveLastItem(Player player){
+        return InteractionResult.PASS;
+    }
+    
+    public InteractionResult tryAddItem(Player player, InteractionHand hand, BlockPos pos){
+        //TODO check if the crafter is full
+        ItemStack stack = player.getItemInHand(hand);
+        if(!stack.isEmpty()){
+            player.level().playSound(player, pos, SoundEvents.ITEM_FRAME_ADD_ITEM, SoundSource.BLOCKS, 1.0f, 1.0f);
+            return InteractionResult.sidedSuccess(level.isClientSide());
+        }
+        return tryRemoveLastItem(player);
     }
 }
