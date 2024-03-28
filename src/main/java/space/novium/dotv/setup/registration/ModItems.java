877c7061c18f5de0493f7d0b298c31f7976f10fe
Novium <nova@novium.space>
2024-03-27 23:42:20 -0600

    Started on mystic seeds
    This is going to be my way of getting all the crops. No more sifting dirt and getting all those nasty pebbles just to try and get plants!

--- src/main/java/space/novium/dotv/setup/registration/ModItems.java
+++ src/main/java/space/novium/dotv/setup/registration/ModItems.java
@@ -6,6 +6,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import space.novium.dotv.DotVMod;
+import space.novium.dotv.world.item.MysticalSeeds;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, DotVMod.MODID);
@@ -14,6 +15,9 @@ public class ModItems {
     public static final RegistryObject<Item> MANA_INFUSED_STONE = ITEMS.register("mana_infused_stone", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> WOODEN_HOE_TOOL = ITEMS.register("wooden_hoe_tool", () -> new Item(new Item.Properties().stacksTo(1)));
     public static final RegistryObject<Item> WOODEN_TOOL_BINDING = ITEMS.register("wooden_tool_binding", () -> new Item(new Item.Properties().stacksTo(1)));
+    
+    //Use Items
+    public static final RegistryObject<Item> MYSTICAL_SEEDS = ITEMS.register("mystical_seeds", () -> new MysticalSeeds(new Item.Properties().stacksTo(64)));
     
     //Block Items
     public static final RegistryObject<Item> STONE_CRAFTER_ITEM = ITEMS.register("stone_crafter", () -> new BlockItem(ModBlocks.STONE_CRAFTER.get(), new Item.Properties()));
---
+++ src/main/java/space/novium/dotv/world/item/MysticalSeeds.java
@@ -0,0 +1,9 @@
+package space.novium.dotv.world.item;
+
+import net.minecraft.world.item.Item;
+
+public class MysticalSeeds extends Item {
+    public MysticalSeeds(Properties properties) {
+        super(properties);
+    }
+}
