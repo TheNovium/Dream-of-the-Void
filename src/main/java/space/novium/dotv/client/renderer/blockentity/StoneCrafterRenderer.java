781462483ca5da33aa5b5f362fdf36f657aabc03
Novium <nova@novium.space>
2024-03-28 00:07:00 -0600

    Crafting stuff
    Try/Catch block in case there are issues with rendering

--- src/main/java/space/novium/dotv/client/renderer/blockentity/StoneCrafterRenderer.java
+++ src/main/java/space/novium/dotv/client/renderer/blockentity/StoneCrafterRenderer.java
@@ -11,6 +11,7 @@ import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import space.novium.dotv.DotVMod;
 import space.novium.dotv.setup.registration.ModRecipeTypes;
 import space.novium.dotv.world.item.crafting.StoneCrafterRecipe;
 import space.novium.dotv.world.item.crafting.recipe.IStoneCrafterRecipe;
@@ -48,15 +49,19 @@ public class StoneCrafterRenderer implements BlockEntityRenderer<StoneCrafterBlo
         Optional<IStoneCrafterRecipe> recipe = entity.getLevel().getRecipeManager().getRecipeFor(ModRecipeTypes.STONE_CRAFTER_TYPE,  entity.getItems(), entity.getLevel());
         if(recipe.isPresent() && recipe.get().matches(entity.getItems(), entity.getLevel())){
             StoneCrafterRecipe r = (StoneCrafterRecipe) recipe.get();
-            ItemStack complete = r.getCompleteItem().getItems()[0];
-            ItemStack result = r.getOutputItem().getItems()[0];
-            pose.pushPose();
-            pose.translate(0.25f, 1.00f, 0.5f);
-            pose.scale(0.25f, 0.25f, 0.25f);
-            renderer.renderStatic(complete, ItemDisplayContext.FIXED, LightTexture.FULL_BRIGHT, overlay, pose, buffer, entity.getLevel(), loc);
-            pose.translate(2.0f, 0.0f, 0.0f);
-            renderer.renderStatic(result, ItemDisplayContext.FIXED, LightTexture.FULL_BRIGHT, overlay, pose, buffer, entity.getLevel(), loc);
-            pose.popPose();
+            try {
+                ItemStack complete = r.getCompleteItem().getItems()[0];
+                ItemStack result = r.getOutputItem().getItems()[0];
+                pose.pushPose();
+                pose.translate(0.25f, 1.00f, 0.5f);
+                pose.scale(0.25f, 0.25f, 0.25f);
+                renderer.renderStatic(complete, ItemDisplayContext.FIXED, LightTexture.FULL_BRIGHT, overlay, pose, buffer, entity.getLevel(), loc);
+                pose.translate(2.0f, 0.0f, 0.0f);
+                renderer.renderStatic(result, ItemDisplayContext.FIXED, LightTexture.FULL_BRIGHT, overlay, pose, buffer, entity.getLevel(), loc);
+                pose.popPose();
+            } catch(Exception e){
+                DotVMod.LOGGER.warning("Failed to find output item for stone crafter!");
+            }
         }
     }
 }
