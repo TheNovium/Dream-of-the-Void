ccc7428862d6a08048efc860f6016583e160f3a4
Novium <nova@novium.space>
2024-03-27 22:11:47 -0600

    StoneCrafter Bug Fix

--- src/main/java/space/novium/dotv/world/item/crafting/StoneCrafterRecipe.java
+++ src/main/java/space/novium/dotv/world/item/crafting/StoneCrafterRecipe.java
@@ -63,7 +63,7 @@ public class StoneCrafterRecipe implements IStoneCrafterRecipe {
                 inputItems.add(itemStack);
             }
         }
-        return inputItems.size() > 0 && inputs.size() == inputItems.size() && RecipeUtil.canCraftWith(inputs, container);
+        return inputs.size() > 0 && inputs.size() == inputItems.size() && RecipeUtil.canCraftWith(inputs, container);
     }
     
     @Override
