b82a593f5ca3afc0b6825c89596c0a68005f9ccb
Novium <nova@novium.space>
2024-03-27 23:18:31 -0600

    Tool time!
    You can now craft a wooden hoe, just in case you get unlucky

--- src/main/java/space/novium/dotv/setup/registration/ModItems.java
+++ src/main/java/space/novium/dotv/setup/registration/ModItems.java
@@ -12,6 +12,8 @@ public class ModItems {
     
     //Items
     public static final RegistryObject<Item> MANA_INFUSED_STONE = ITEMS.register("mana_infused_stone", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> WOODEN_HOE_TOOL = ITEMS.register("wooden_hoe_tool", () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final RegistryObject<Item> WOODEN_TOOL_BINDING = ITEMS.register("wooden_tool_binding", () -> new Item(new Item.Properties().stacksTo(1)));
     
     //Block Items
     public static final RegistryObject<Item> STONE_CRAFTER_ITEM = ITEMS.register("stone_crafter", () -> new BlockItem(ModBlocks.STONE_CRAFTER.get(), new Item.Properties()));
